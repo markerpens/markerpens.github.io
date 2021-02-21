4d64c62cfb604ebe7a2b0bfa62535aa8b37a0c0c
Mark <murkrobles@gmail.com>
2021-02-20 19:51:04 -0800

    Updating Resume
    - Added up + down buttons
    - Updated resume
    - Added next project button in footer
    - Fleshed out details in process, etc.

--- hide.js
+++ hide.js
@@ -1,8 +1,32 @@
-// $(document).ready(function() {
-//     $(".divs").hide();
+$(document).ready(function () {
 
-//     $(".buttons").click(function() {
-//         $(".divs").hide().eq($(this).index()).show();
-//     });
-// });
+    (function ($) {
 
+        /*--Scroll Back to Top Button Show--*/
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+
+        // Click event scroll to top button jquery
+
+        $('#back-to-top').click(function () {
+
+            $('html, body').animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+
+        // Down arrow scroll to anchor
+
+        $(".down-arrow").click(function (e) {
+            e.preventDefault();
+            var aid = $(this).attr("href");
+            $('html,body').animate({ scrollTop: $(aid).offset().top }, 'slow');
+        });
+
+    })(jQuery);
+});
