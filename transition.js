392fc1a90b84eee5b622d1199a84afab6a5b162c
Mark R <murkrobles@gmail.com>
2025-04-01 16:07:09 -0700

    add fade in effect to all pages
    clean up js

--- transition.js
+++ transition.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Directly show the content with fade-in and slide effect
+    // Wait until the page is loaded, then apply the transition
     setTimeout(() => {
         document.querySelector('.content').classList.add('show');
-    }, 500);  // 0.5 seconds, adjust for delay if needed
+    }, 500);  // A small delay to ensure the page is fully loaded
 });
