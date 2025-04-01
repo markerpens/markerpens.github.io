34bdc7dbc97703dd42495ee60e020eaee6d58966
Mark R <murkrobles@gmail.com>
2025-04-01 16:16:17 -0700

    update fadein effect and timing

--- transition.js
+++ transition.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
     // Wait until the page is loaded, then apply the transition
     setTimeout(() => {
         document.querySelector('.content').classList.add('show');
-    }, 500);  // A small delay to ensure the page is fully loaded
+    }, 800);  // A small delay to ensure the page is fully loaded
 });
